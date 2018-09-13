0c94ce8cd384186f43f7e6976628af851a42285a
vpsg_ <vpsg_@DESKTOP-JNRQB80>
2018-09-13 01:59:38 -0300

    01 CONTROLLER A MAIS

---
+++ LocadoraVeiculos/src/controllers/ControllerMenu.java
@@ -0,0 +1,217 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ControllerMenu {
+
+	@FXML
+	private TableColumn<?, ?> tcStatus;
+
+	@FXML
+	private Button btEditFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcCor;
+
+	@FXML
+	private TableColumn<?, ?> tcDataInicio;
+
+	@FXML
+	private TableColumn<?, ?> tcSituacao;
+
+	@FXML
+	private TableColumn<?, ?> tcPlaca;
+
+	@FXML
+	private TableColumn<?, ?> tcEnderecoFilial;
+
+	@FXML
+	private Button btExcluirFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcIdade;
+
+	@FXML
+	private TableColumn<?, ?> tcTelefoneFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcIdHistoricoAluguel;
+
+	@FXML
+	private TableColumn<?, ?> tcValorTotal;
+
+	@FXML
+	private TableColumn<?, ?> tcIdAluguel;
+
+	@FXML
+	private Button btExcluirCliente;
+
+	@FXML
+	private TableColumn<?, ?> tcHistoricoSituacao;
+
+	@FXML
+	private Button btNewCliente;
+
+	@FXML
+	private Button btExcluirAluguel;
+
+	@FXML
+	private TableColumn<?, ?> tcNomeCliente;
+
+	@FXML
+	private TableColumn<?, ?> tcFabricante;
+
+	@FXML
+	private Button btExcluirVeiculo;
+
+	@FXML
+	private TableColumn<?, ?> tcEmail;
+
+	@FXML
+	private TableColumn<?, ?> tcLocando;
+
+	@FXML
+	private TableColumn<?, ?> tcCidadeFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcIdCliente;
+
+	@FXML
+	private TableColumn<?, ?> tcAnoFabricacao;
+
+	@FXML
+	private TableColumn<?, ?> tcTelefoneCliente;
+
+	@FXML
+	private TableColumn<?, ?> tcCPF;
+
+	@FXML
+	private TableColumn<?, ?> tcLocatario;
+
+	@FXML
+	private Button btCadastrarVeiculos;
+
+	@FXML
+	private TableColumn<?, ?> tcHistoricoDataInicio;
+
+	@FXML
+	private TableColumn<?, ?> tcDataFim;
+
+	@FXML
+	private Button btEditCliente;
+
+	@FXML
+	private Button btEditVeiculo;
+
+	@FXML
+	private Button btNewFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcCarroAlugado;
+
+	@FXML
+	private TableColumn<?, ?> tcRG;
+
+	@FXML
+	private TableColumn<?, ?> tcHistoricoLocatario;
+
+	@FXML
+	private Button btEditAluguel;
+
+	@FXML
+	private TableColumn<?, ?> tcHistoricoValorTotal;
+
+	@FXML
+	private TableColumn<?, ?> tcPropretario;
+
+	@FXML
+	private TableColumn<?, ?> tcHistoricoDataFim;
+
+	@FXML
+	private TableColumn<?, ?> tcHistoricoCarroAlugado;
+
+	@FXML
+	private Button btNewAluguel;
+
+	@FXML
+	private TableColumn<?, ?> tcIdFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcNomeFilial;
+
+	@FXML
+	private TableColumn<?, ?> tcIdVeiculo;
+
+	@FXML
+	private TableColumn<?, ?> tcEstado;
+
+	@FXML
+	private TableColumn<?, ?> tcModelo;
+
+	@FXML
+	private TableColumn<?, ?> tcCNH;
+
+	@FXML
+	void criarNovoAluguel(ActionEvent event) {
+
+	}
+
+	@FXML
+	void editarAluguel(ActionEvent event) {
+
+	}
+
+	@FXML
+	void excluirAluguel(ActionEvent event) {
+
+	}
+
+	@FXML
+	void cadastrarVeiculos(ActionEvent event) {
+
+	}
+
+	@FXML
+	void editarVeiculo(ActionEvent event) {
+
+	}
+
+	@FXML
+	void excluirVeiculo(ActionEvent event) {
+
+	}
+
+	@FXML
+	void adicionarCliente(ActionEvent event) {
+
+	}
+
+	@FXML
+	void editarCliente(ActionEvent event) {
+
+	}
+
+	@FXML
+	void excluirCliente(ActionEvent event) {
+
+	}
+
+	@FXML
+	void novaFilial(ActionEvent event) {
+
+	}
+
+	@FXML
+	void excluirFilial(ActionEvent event) {
+
+	}
+
+	@FXML
+	void editarFilial(ActionEvent event) {
+
+	}
+
+}
