98fe8eb621c3a725d225030884b371e357705586
vpsg_ <vpsg_@DESKTOP-JNRQB80>
2018-09-13 00:17:29 -0300

    Já abre a tela de login

--- LocadoraVeiculos/src/principal/Main.java
+++ LocadoraVeiculos/src/principal/Main.java
@@ -1,10 +1,42 @@
 package principal;
 
-public class Main {
+import java.sql.SQLException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application {
 
+	public static BorderPane root;
+
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			root = (BorderPane) FXMLLoader.load(getClass().getResource("/telas/TelaPai.fxml"));
+			Scene scene = new Scene(root, 1000, 650);
+			scene.getStylesheets().add(getClass().getResource("/telas/Style.css").toExternalForm());
+			primaryStage.setTitle("Login");
+			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("file:/Imagens/imagembackground.jpg"));
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/telas/TelaLogin.fxml"));
+			AnchorPane agenciaView = (AnchorPane) loader.load();
+			root.setCenter(agenciaView);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws SQLException {
+		launch(args);
+	}
+
+	public static BorderPane getRoot() {
+		return root;
 	}
 
 }
